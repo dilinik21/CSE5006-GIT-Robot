8da24e5b857292fa79f2d983f516923b57da2a8e
Dilini Karunarathna <dilinirasanjana95@gmail.com>
2025-08-23 21:43:00 +1000

    Hamburger menu

---
+++ app/components/HamburgerMenu.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState } from "react";
+import styles from "./HamburgerMenu.module.css";
+
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div>
+      {/* Hamburger Icon */}
+      <div
+        className={styles["hamburger-menu"]}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className={styles["hamburger-line"]}></div>
+        <div className={styles["hamburger-line"]}></div>
+        <div className={styles["hamburger-line"]}></div>
+      </div>
+
+      {/* Menu Overlay */}
+      {isOpen && (
+        <div className={styles["menu-overlay"]}>
+          <a href="/" className={styles["menu-item"]}>
+            Home
+          </a>
+          <a href="/docker" className={styles["menu-item"]}>
+            Docker
+          </a>
+          <a href="/prisma" className={styles["menu-item"]}>
+            Prisma
+          </a>
+          <a href="/tests" className={styles["menu-item"]}>
+            Tests
+          </a>
+          <a href="/about" className={styles["menu-item"]}>
+            About
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HamburgerMenu;
